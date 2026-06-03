803baf249b3bfb1bc72186d485c173a75857d7ef
Ahmiii281 <ba341914@gmail.com>
2026-06-03 11:03:29 +0500

    fix: styling error

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import compression from "vite-plugin-compression";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     compression({ algorithm: "brotliCompress", ext: ".br" }),
     compression({ algorithm: "gzip", ext: ".gz" })
